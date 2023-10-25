0d5055584a78b4c63ee051a4f4814b173537b16d
r-work <rebin@mitransit.co.uk>
2023-10-25 17:25:51 +0100

    init

---
+++ MauiCrash/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiCrash
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
